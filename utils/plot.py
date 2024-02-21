3f830c740f749e5627318c4d50dd8f0bff9a4b12
Napat Sahapat <n.sahapat@outlook.com>
2024-02-21 16:37:30 +1100

    Plot specific rep_names instead of collating within run_names

--- utils/plot.py
+++ utils/plot.py
@@ -17,22 +17,25 @@ def q3(a):
     return np.quantile(a, q=0.75)
 
 
-def plot(parent_dir, run_base_name, run_ids,
+def plot(parent_dir, run_base_name, run_ids, rep_name=None,
          agent_names=None, save=False, sort=False,
-         scalars=("ep_rew_mean"), time_units="total_timesteps",
+         scalars=("ep_rew_mean",), time_units="total_timesteps",
          bin_width=2000, changing_var=None,
          plot_title=None, legend_title=None, labels=None):
 
+    ##########
+    # Metadata
+    ##########
 
     run_names = [f"{run_base_name}_{run_id}" for run_id in run_ids]
     collated_run_names = f"{run_base_name}_{'-'.join(map(str, run_ids))}"
-
 
     agent_names = ["predator"] if agent_names is None else agent_names
     # Future
     # agent_names = extract_config(parent_dir=parent_dir, run_name=run_names[0],
     #                              name="agent_names")
 
+    # Custom labelling by variable name grouping
     if changing_var is not None:
         variable = None
         if isinstance(changing_var, str):
@@ -60,11 +63,12 @@ def plot(parent_dir, run_base_name, run_ids,
         plot_title = parent_dir
         legend_title = run_base_name
 
-
     # variable = changing_var
 
 
-
+    ##############
+    # Collate data
+    ##############
 
     df_dict = {name: [] for name in agent_names}
 
@@ -72,16 +76,23 @@ def plot(parent_dir, run_base_name, run_ids,
 
         df_run_dict = {name: [] for name in agent_names}
 
+        # Collect csv files from matching "agent_names" and "rep_names"
         for dirpath, dirnames, filenames in os.walk(os.path.join("logs", parent_dir, run_name)):
             current_dir = os.path.split(dirpath)[-1]
+            above_current_dir = str.split(dirpath, os.path.sep)[-2]
 
             if current_dir in agent_names:
-                csv_file = next(filter(lambda x: x.endswith("csv"), filenames))
-                df = pd.read_csv(os.path.join(dirpath, csv_file))
-                df_run_dict[current_dir].append(df)
+                if above_current_dir in rep_name or rep_name is None:
+                    # Read file and append to dictionary
+                    csv_file = next(filter(lambda x: x.endswith("csv"), filenames))
+                    df = pd.read_csv(os.path.join(dirpath, csv_file))
+                    df_run_dict[current_dir].append(df)
+                else:
+                    continue
             else:
                 continue
 
+        # Collate data from same "agent_names" across "rep_names"
         for agent in agent_names:
             run_df = pd.concat(df_run_dict[agent], keys=[*range(1, len(df_run_dict[agent]) + 1)],
                                names=["run_ids", "row_id"])
@@ -89,6 +100,7 @@ def plot(parent_dir, run_base_name, run_ids,
 
             run_df = run_df.loc[:, [time_units, *scalars]]
 
+            # Bin x-axis across reps/samples according to bin-width
             # Only bin if bin width if much smaller than time units (for timesteps)
             if time_units == "total_timesteps":
                 run_df[time_units] = run_df[time_units].apply(lambda x: x // bin_width * bin_width)
@@ -100,17 +112,22 @@ def plot(parent_dir, run_base_name, run_ids,
 
             df_dict[agent].append(run_df)
 
+    # Collate across "run_ids" for same "agent_names"
     for agent in agent_names:
         combined_df = pd.concat(df_dict[agent], keys=run_names)
         df_dict[agent] = combined_df
 
-    # Plot
+
+    ##########
+    # Plotting
+    ##########
+
     # cmap = mpl.colormaps["viridis"]
     cmap = mpl.colormaps.get_cmap("Set2")
     # colors = cmap(np.linspace(0,1,num=len(run_names)))
     colors = cmap.colors[:len(run_names)]
 
-
+    # Separate plots for each "agent_names"
     for agent in agent_names:
         for scalar in scalars:
             fig, ax = plt.subplots(figsize=(15, 10))
@@ -140,7 +157,8 @@ def plot(parent_dir, run_base_name, run_ids,
 if __name__ == "__main__":
     parent_dir = "test1"
     run_base_name = "TestAlgo"
-    run_ids = [26,27]
+    run_ids = [24]
+    rep_name = ["DQN_1", "DQN_3"]
     # changing_var = None
     sort = False
     save = False
@@ -158,7 +176,7 @@ if __name__ == "__main__":
     time_units = "total_timesteps"
     bin_width = 2000
 
-    plot(parent_dir, run_base_name, run_ids,
+    plot(parent_dir, run_base_name, run_ids, rep_name,
          agent_names=["predator1"],
          scalars=scalars, changing_var=changing_var,
          plot_title=plot_title, legend_title=legend_title, labels=labels)
