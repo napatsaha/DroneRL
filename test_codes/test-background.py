472b7403ccfef07fdbb4aefc58dc4a5ac6ccebec
Napat Sahapat <n.sahapat@outlook.com>
2024-04-08 19:14:49 +1000

    plot_eval_result: plot_info_background()

---
+++ test_codes/test-background.py
@@ -0,0 +1,43 @@
+from analysis.plot_eval_result import plot_info_background
+from utils.plot_utils import draw_background
+from utils.config import get_config
+import matplotlib.pyplot as plt
+import numpy as np
+import string
+
+# Metadata
+parent_dir = "test2"
+run_base_name = "TestWorked"
+run_ids = [1, 3, 4, 5, 8]
+max_reps = 10
+metric = None
+save = False
+show = True
+
+# plot_info_background(
+#     parent_dir,
+#     run_base_name,
+#     run_ids
+# )
+
+canvas = draw_background(parent_dir, run_base_name, run_ids[-1])
+cmap = plt.get_cmap("Set1")
+
+fig, ax = plt.subplots(figsize=(10,10))
+ax.imshow(canvas, cmap=plt.get_cmap("gray"), extent=(0, 1, 0, 1), origin="lower")
+
+
+num_rects = 11
+for i in range(num_rects):
+    xy = np.random.rand(2) * 0.8
+    width = 0.2 # np.random.rand() * 0.4 + 0.1
+    length = 0.2 # np.random.rand() * 0.4 + 0.1
+    rect = plt.Rectangle(xy, width, length, ec=cmap(i), fc=cmap(i, 0.1))
+    ax.add_patch(rect)
+    ax.annotate(f"{string.ascii_uppercase[i]}", xy + 0.5 * np.array([width, length]),
+                xytext=xy + np.array([0.75*width, 1.2 * length]),
+                ha="left", color=cmap(i), fontsize=20,
+                arrowprops=dict(color="grey", connectionstyle="arc3,rad=-0.25", arrowstyle= "fancy,tail_width=0.1,head_width=0.4"))
+
+# plt.axis("off")
+plt.show()
--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -1,4 +1,7 @@
+import numpy as np
 
+from utils import env as env_utils
+from utils.config import get_config
 
 ACTION_DICT_CARDINAL = \
     {0: "Stationary",
@@ -6,3 +9,25 @@ ACTION_DICT_CARDINAL = \
      2: "Left",
      3: "Down",
      4: "Right"}
+
+
+def draw_background(parent_dir, run_base_name, run_id) -> np.ndarray:
+    """
+    Quick function to draw base background of a certain environment.
+
+    Parameters
+    ----------
+    parent_dir :
+    run_base_name :
+    run_id :
+
+    Returns
+    -------
+
+    """
+    config = get_config(parent_dir, run_base_name, run_id)
+    env = env_utils.create_env(config)
+
+    canvas = env.draw_canvas(draw_agents=False, return_canvas=True)
+    canvas = canvas.canvas.T
+    return canvas
